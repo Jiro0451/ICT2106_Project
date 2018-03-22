63668db62cc98d4255c03128476d7307c71ee55c
Jiro0451 <jchan0451@gmail.com>
2018-03-22 14:17:29 +0800

    Initial commit

---
+++ ICT2106_Project/ICT2106_Project/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ICT2106_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
